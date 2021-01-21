2fbc412a0e871661d20a6429fa6d8bdef38befa8
junm.24 <36562572+junm24@users.noreply.github.com>
2021-01-20 23:43:41 -0500

    Update 1-20-21 Usaco bronze bucket brigade.py

--- 1-20-21 Usaco bronze bucket brigade.py
+++ 1-20-21 Usaco bronze bucket brigade.py
@@ -11,5 +11,4 @@ for i in range(len(layout)):
             barn = [i+1, j+1]
         elif row[j] == "L":
             lake = [i+1, j+1]
-# if rock[0] not in list(range(min(barn[0], lake[0]), max(barn[0], lake[0]))) and rock[1] not in list(range(min(barn[1], lake[1]), max(barn[1], lake[1]))):
     print(len(list(range(min(barn[0], lake[0]), max(barn[0], lake[0])))) + len(list(range(min(barn[1], lake[1]), max(barn[1], lake[1])))) - 1)
